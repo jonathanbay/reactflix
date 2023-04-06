46a16b9587aef2fec289f8882d363e58bd56efd9
jonathan <bayart.jonathan@laposte.net>
2023-04-06 10:20:05 +0200

    ajout du menu de navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Favoris from "./pages/Favoris";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coups-de-coeur" element={<Favoris />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
