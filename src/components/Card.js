dcfdba7871a395e5313d2985720d516fc130825b
jonathan <bayart.jonathan@laposte.net>
2023-04-29 16:31:13 +0200

    ajout page favoris

--- src/components/Card.js
+++ src/components/Card.js
@@ -75,6 +75,22 @@ const Card = ( {movie}) => {
         return genreArray.map((genre) => <li key={genre}>{genre}</li>);
     };
 
+    const addStorage = () => {
+        let storedData = window.localStorage.movies ? window.localStorage.movies.split(", ") : [];
+
+        if(!storedData.includes(movie.id.toString())) {
+            storedData.push(movie.id);
+            window.localStorage.movies = storedData;
+        }
+    };
+
+    const deleteStorage = () => {
+        let storedData = window.localStorage.movies.split(",");
+        let newData = storedData.filter((id) => id != movie.id);
+        window.localStorage.movies = newData;
+    }
+
+
     return (
         <div className='card'>
             <img src= {movie.poster_path ?
@@ -85,13 +101,21 @@ const Card = ( {movie}) => {
             {movie.release_date ? 
                 <h5>Sortie le : { dateFormater(movie.release_date) }</h5> : null
             }
-            <h4>{movie.vote_average}/10 <span>⭐</span> </h4>
+            <h4>{movie.vote_average.toFixed(1)}/10 <span>⭐</span> </h4>
             <ul>
-                {movie.genre_ids ? genreFinder() : null}
+                {movie.genre_ids ? genreFinder() : movie.genres.map((genre) => <li key={genre}>{genre.name}</li>)}
             </ul>
             {movie.overview ? <h3>Synopsis</h3> : ""}
             <p>{movie.overview}</p>
-            <div className='btn'>Coups de coeur</div>
+            {movie.genre_ids ? (
+                <div className='btn' onClick={() => addStorage()}>Coups de coeur</div>
+            ) : (
+                <div className='btn' onClick={() => {
+                    deleteStorage();
+                    window.location.reload();
+                }}>Supprimer de la liste</div>
+            )
+        }
         </div>
     );
 };
--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import axios from 'axios';
+import Card from '../components/Card';
 
 const Favoris = () => {
+    const [listData, setListData] = useState([]);
+
+    useEffect(() => {
+
+        // let movieArray = [];
+
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(",") : [];
+
+        for(let i = 0; i < moviesId.length; i++) {
+            axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=e060479fc192cc1c6560b723a60fae13&language=fr-FR`)
+            .then((res) => setListData((listData) => [...listData, res.data]));
+        }
+    }, []);
+
     return (
         <div className='user-list-page'>
             <Header />
+            <h2>Coups de coeur <span>❤️</span></h2>
+            <div className='result'>
+                {listData.length > 0 ? (
+                    listData.map((movie) => <Card movie={movie} key={movie.id} />)) : (<h2>Aucun coups de coeur pour le moment</h2>
+                )}
+            </div>
         </div>
     );
 };
